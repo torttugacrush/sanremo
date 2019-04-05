879bc069a81229ff8db7287c86a094037dea4b22
torttugacrush <ibanezmarcos0@gmail.com>
2019-04-05 03:36:13 -0500

    Create index.php

---
+++ ejemplos/ejemplo1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php 
+	print "Bienvenidos al curso de php <br>";
+	print "Hola gente <br>";
+	print "Hasta el proximo video";
+
+?>
+	
+</body>
+</html>
