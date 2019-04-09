494d771f86fa818a2f0fd5a649513310410f0921
torttugacrush <ibanezmarcos0@gmail.com>
2019-04-09 04:27:21 -0500

    WAD

---
+++ ejemplos/phpPOO/poo_1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include("vehiculos.php");
+$mazda = new Coche();
+
+$pegaso=new Camion();
+echo "el maszda tiene " . $mazda->ruedas . " ruedas <br>";
+
+echo "el pedaso tiene " . $pegaso->ruedas . " ruedas <br>";
+
+
+
+
+?>
+
+</body>
+</html>
---
+++ ejemplos/phpPOO/vehiculos.php
@@ -0,0 +1,85 @@
+<?php
+    class Coche {
+        var $ruedas;
+        var $color;
+        var $motor;
+
+        function Coche(){ 
+            $this->ruedas=4;
+            $this->color="";
+            $this->motor=1600;
+        }
+
+        function arrancar(){
+
+            echo "estoy arrancando <br>";
+
+            
+        }
+        function girar(){
+
+            echo "Eestoy girando <br>";
+
+        }
+        function frenar(){
+
+            echo"estoy frenando <br>";
+
+        }
+        function establece_color($colorCoche,$nombreCoche){
+            $this->color=$colorCoche;
+
+            echo "El color de" .  $nombreCoche . "es: " . $this->color . "<br>";
+        }
+    }
+
+    class Camion {
+        var $ruedas;
+        var $color;
+        var $motor;
+
+        function Camion(){ 
+            $this->ruedas=8;
+            $this->color="gris";
+            $this->motor=2600;
+        }
+
+        function arrancar(){
+
+            echo "estoy arrancando <br>";
+
+            
+        }
+        function girar(){
+
+            echo "Eestoy girando <br>";
+
+        }
+        function frenar(){
+
+            echo"estoy frenando <br>";
+
+        }
+
+      
+     /*   function establece_color($colorCamion,$nombreCamion){
+            $this->color=$colorCamion;
+
+            echo "El color de" .  $nombreCamion . "es: " . $this->color . "<br>";
+        }*/
+    }
+  /* $renault = new Coche();
+   
+   $mazda = new Coche();
+
+   $seat=new Coche();
+
+   $renault->establece_color("rojo" , "Renault");
+
+   $seat->establece_color("azul","Seat");
+
+   //$mazda->girar();
+
+   //echo $mazda->ruedas;
+*/
+    ?>
